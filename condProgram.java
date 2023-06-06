e6537c156fa5fedbf881793a836be5ffe661b146
Prasad <kundan.a.prasad@capgemini.com>
2023-06-06 13:28:08 +0530

    changes made

--- condProgram.java
+++ condProgram.java
@@ -20,3 +20,21 @@ public class Main
 		
 	}
 }
+
+																				OR
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		if(a<b && a<c) {
+		    System.out.println("A is least");
+		} else if(b<c) {
+		    System.out.println("B is least");
+		} else {
+		    System.out.println("C is least");
+		}
+	}
+}
