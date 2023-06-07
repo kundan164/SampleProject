46f5ef9e970e8396a1539cf55f93f55c7e8ee7c5
Prasad <kundan.a.prasad@capgemini.com>
2023-06-07 12:45:40 +0530

    new changes

--- condProgram.java
+++ condProgram.java
@@ -38,3 +38,37 @@ public class Main
 		}
 	}
 }
+
+
+=>
+public class Main
+{
+	public static void main(String[] args) {
+// 		int a = 10;
+// 		int b = 20;
+		
+// 		int c = (a<b)? a:b;
+// 		System.out.println(c);
+
+        int a = 100;
+        int b = 20;
+        int c = 60;
+        
+        int res = (a<b)? (a<c? a:c):(b<c?b:c);
+        System.out.println(res);
+        
+        int num1 = 100;
+        int num2 = 20;
+        int num3 = 60;
+        
+        if(num1<num2) {
+            if(num1<num3) {
+            System.out.println(num1);
+            }
+        } else if(num2<num3) {
+            System.out.println(num2);
+        } else {
+            System.out.println(num3);
+        }
+	}
+}
