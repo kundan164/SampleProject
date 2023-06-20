1e1cb623bbb8c791f8c8a18803f9951ef277a8d3
Prasad <kundan.a.prasad@capgemini.com>
2023-06-20 15:43:32 +0530

    New Changes

--- condProgram.java
+++ condProgram.java
@@ -72,3 +72,28 @@ public class Main
         }
 	}
 }
+
+
+
+Pattern Programming:-
+------------------------
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n = 4;
+	    
+	    for(int i=0; i<4; i++) {                                    ****
+	        for(int j=0; j<n; j++) {                                *  *
+	            if(i==0 || i==(n-1) || j==0 || j==(n-1)) {     ==>  *  *
+	                System.out.print("*");                          ****
+	            } else {
+	                System.out.print(" ");
+	            }
+	        }
+	        System.out.println();
+	    }
+	}
+}
+
+
