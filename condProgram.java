1ee9e24120a6f6bdaac2c1854241cd7b40905c15
Prasad <kundan.a.prasad@capgemini.com>
2023-06-29 13:25:22 +0530

    changes made

--- condProgram.java
+++ condProgram.java
@@ -129,4 +129,48 @@ public class Main
 	}
 }
 
+N-Pattern :-
+----------
+
+public class Main
+{
+	public static void main(String[] args) {
+	   int n = 10;
+		
+		for(int i=0; i<n; i++) {
+		    for(int j=0; j<n; j++) {
+		        if(i==j || j==0 || j==(n-1)) {
+		            System.out.print("*");
+		        } else {
+		            System.out.print(" ");
+		        }
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	   int n = 28;
+		
+		for(int i=0; i<n; i++) {
+		    for(int j=0; j<n; j++) {
+		        if(j==0 || i+j==(n-1)/2 || i-j==(n-1)/2
+		          || j-i==(n-1)/2 || i+j==n-1+((n-1)/2)
+		          || i==j || i+j==n-1
+		          || i==0 || j==0 || i==n-1 || j==n-1) {
+		            System.out.print("*");
+		        } else {
+		            System.out.print(" ");
+		        }
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
 
